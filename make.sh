7ff7b07d84e46bb5058df81d924aa2911d194cc8
Hyenadae <hypprs@gmail.com>
2020-07-18 18:38:52 -0300

    feat(boinc): initial boinc_api inclusion

--- make.sh
+++ make.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env sh
 cargo build --release || exit 1
-cp target/release/libkaktwoos_rs.so bin
-g++ -O3 -o bin/a.out main.cc bin/libkaktwoos_rs.so -Wall -Wextra || exit 1
+mkdir ./bin
+cp target/release/libkaktwoos_rs.so ./bin/libkaktwoos_rs.so
+
+g++ -m64 -O3 -o bin/kaktwoos.out main.cc bin/libkaktwoos_rs.so -Wall -Wextra -Iboinc/ -Lboinc/lib/lin -static-libgcc -static-libstdc++ -lboinc_api -lboinc -lboinc_opencl -pthread -Wl,-dynamic-linker,/lib64/ld-linux-x86-64.so.2 || exit 1
